c02b2309f6654dbbd210fc530ced2e2c38b63429
Redarokim Ikongha <rikongha@gmail.com>
2021-10-07 16:57:53 +0100

    initial project setup

---
+++ src/main/java/com/rikongha/kalaha/KalahaApplication.java
@@ -0,0 +1,13 @@
+package com.rikongha.kalaha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KalahaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KalahaApplication.class, args);
+	}
+
+}
